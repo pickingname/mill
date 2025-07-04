71e6b05ca704933d580749c3ea8708b1856c6e11
pickingname <trintrinir@icloud.com>
2025-07-04 21:57:11 +0700

    feat: sums up the infoBox implementation

--- src/utils/map/internal/internalBound.js
+++ src/utils/map/internal/internalBound.js
@@ -1,11 +1,12 @@
-import {config} from "../../config";
-import {map} from "../initMap.js";
+import { config } from "../../config";
+import { map } from "../initMap.js";
 
 export function internalBound(bound) {
   map.fitBounds(bound, {
     padding: config.map.bound_padding,
     duration: config.map.bound_duration,
     easing: (t) => 1 - Math.pow(1 - t, 5),
-    linear: true
+    linear: true,
+    maxZoom: 8,
   });
 }
