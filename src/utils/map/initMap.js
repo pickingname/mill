0571c8ab0b9882ab2eea5ce516e7bc7183ca079e
pickingname <trintrinir@icloud.com>
2025-07-12 16:58:14 +0700

    feat: add terrain support to map initialization

--- src/utils/map/initMap.js
+++ src/utils/map/initMap.js
@@ -63,6 +63,16 @@ export function initMap() {
   map.dragRotate.disable();
   map.touchZoomRotate.disableRotation();
 
+  map.on("style.load", () => {
+    map.addSource("mapbox-dem", {
+      type: "raster-dem",
+      url: "mapbox://mapbox.mapbox-terrain-dem-v1",
+      tileSize: 512,
+      maxzoom: 14,
+    });
+    map.setTerrain({ source: "mapbox-dem", exaggeration: 1 });
+  });
+
   map.on("load", async () => {
     map.resize();
     startMainLoop();
