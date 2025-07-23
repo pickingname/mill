76dd7335fa08faf3d3948024cfdf73e12d1d0963
pickingname <trintrinir@icloud.com>
2025-07-23 19:33:46 +0700

    feat: update to use testData

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,7 +6,7 @@ export const config = {
   },
   api: {
     base_url: import.meta.env.DEV
-      ? "http://127.0.0.1:1212/p2p"
+      ? "/assets/map/testData.json"
       : "https://api.p2pquake.net/v2/history?codes=551&limit=1", // 60 リクエスト/分 (IP アドレス毎)
     interval: 5000,
 
